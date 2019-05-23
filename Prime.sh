06e26f26dd269a16016fdecb7866ac4a4c65369e
sudingquan <1151015256@qq.com>
2019-05-23 21:04:37 +0800

    modify Prime.sh

--- Prime.sh
+++ Prime.sh
@@ -3,7 +3,7 @@ for i in `seq 0 1000`; do
     prime[i]=0
 done
 for ((i=2;$[ i*i ]<=1000;i++)); do
-    if [[ prime[i] -eq 1 ]]; then
+    if [[ ${prime[${i}]} -eq 1 ]]; then
         continue
     fi
     for((j=$[ i*i ];j<=1000;j+=i)); do 
@@ -12,7 +12,7 @@ for ((i=2;$[ i*i ]<=1000;i++)); do
 done
 ans=0
 for i in `seq 2 1000`; do
-    if [[ prime[i] -eq 0 ]]; then
+    if [[ ${prime[${i}]} -eq 0 ]]; then
         echo ${i}
         ans=$[${ans}+i]
     fi
